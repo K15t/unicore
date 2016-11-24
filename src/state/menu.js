ba1e8a130881e8ef2972a3154481a32cf90333ef
Thomas Hirsch <thomas@k15t.com>
2016-11-24 18:20:26 +0100

    Added minimal main menu that switches the state.

--- src/state/menu.js
+++ src/state/menu.js
@@ -1,4 +1,3 @@
-
 export default class Menu extends Phaser.State {
 
     init() {
@@ -10,16 +9,28 @@ export default class Menu extends Phaser.State {
     }
 
     create() {
+        const game = this.game;
 
+        const startButton = game.add.button(game.world.centerX - 100, 200, 'startButton', this.gotoGameState.bind(this));
+        const highscoreButton = game.add.button(game.world.centerX - 100, 400, 'highscoreButton', this.gotoHigscoreState.bind(this), 0, 1, 2);
     }
 
     update() {
         // go to different state with this.state.start()
-        this.state.start('game');
+        //this.state.start('game');
     }
 
     shutdown() {
 
+    }
+
+
+    gotoGameState() {
+        this.state.start('game');
+    }
+
+    gotoHigscoreState() {
+        this.state.start('highscore');
     }
 
 }
