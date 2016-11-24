2f721186a81ab95acfeb596f0fd8c4007f4b81c4
Martin Muzatko <martin@happy-css.com>
2016-11-24 16:34:53 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# riot-stack
+A complete stack to help you build webapps
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: './',
+        filename: '[name].js',
+        sourceMapFilename: "[name].js.map",
+    },
+    module: {
+        preLoaders: [
+            { test: /\.js$/, loader: 'source-map' },
+        ],
+        loaders: [
+            { test: /\.(jpe?g|png|gif|svg|mp4)$/i, loader: 'file'},
+            { test: /\.html$|\.js$/, loader: 'babel', query: { presets: 'es2015' }},
+            { test: /\.less$/, loader: 'style!css!postcss!less'},
+            { test: /\.json$/, loader: 'json'},
+        ]
+    },
+    devServer: {
+        port: 8080,
+        inline: true
+    },
+    eslint: {
+        configFile: './.eslintrc'
+    },
+    devtool: 'source-map'
+}
