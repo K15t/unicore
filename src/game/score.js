532bf2ca573a6ce9aac24b3be36826ea1e32beb4
Martin Muzatko <martin@happy-css.com>
2016-11-25 11:15:34 +0100

    Call REST URL upon gameover to display scores

--- src/game/score.js
+++ src/game/score.js
@@ -1,4 +1,6 @@
 import leftPad from 'left-pad'
+import request from 'browser-request'
+import constants from '../constants';
 
 export default class Score {
     constructor(game) {
@@ -10,7 +12,7 @@ export default class Score {
         scoreBar.drawRect(0,0,game.width,game.height*.1)
         scoreBar.endFill()
 
-        this.scoreCounter = game.add.text(game.width*.5,game.height*.025, ''+this.score, {fill:'#fff', boundsAlignV:'center'})
+        this.scoreCounter = game.add.text(game.width*.5,game.height*.025, this.getScore(), {fill:'#fff', boundsAlignV:'center'})
     }
 
     add(amount) {
@@ -18,10 +20,21 @@ export default class Score {
     }
 
     update() {
-        this.scoreCounter.text = 'SCORE - ' + leftPad(this.score, 10, 0)
+        this.scoreCounter.text = this.getScore()
+    }
+
+    getScore() {
+        return 'SCORE - ' + leftPad(this.score, 10, 0)
+    }
+
+    getScores() {
+        this.game.add.text()
+        request.get(`${constants.REST_URL}?_sort=score&_order=DESC&_limit=10`, (error, response, body)=>{
+            console.log(body);
+        })
     }
 
     upload() {
-        
+
     }
 }
--- src/state/highscore.js
+++ src/state/highscore.js
@@ -1,6 +1,6 @@
+import BaseState from './base';
 
-
-export default class Highscore extends Phaser.State {
+export default class Highscore extends BaseState {
 
     init() {
 
@@ -14,13 +14,15 @@ export default class Highscore extends Phaser.State {
         super.create();
 
         this.isPlaying = false;
+        this.score.getScores()
 
         const game = this.game;
 
-        const startButton = game.add.button(game.world.centerX - 100, 200, 'startButton', this.gotoGameState.bind(this));
+        const startButton = game.add.button(game.world.centerX - 100, 200, 'startButton', ()=>{this.state.start('game')});
     }
 
     update() {
+        super.update()
         // go to different state with this.state.start()
         // this.state.start('game');
     }
